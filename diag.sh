b0c9a6338689242c3a6838ca95c7e0abf495815d
Josh C <joshc@marvin.local>
2013-08-26 22:51:25 -0500

    added port scan

--- diag.sh
+++ diag.sh
@@ -12,8 +12,14 @@ function doLookup {
   nslookup -type=ANY $1
 }
 
+function doPortScan {
+  nmap -p T:22,25,80,443,4443,8000,8080 $1
+}
+
 echo "DNS info:"
 doLookup $1
 echo "=========="
 doPing $1
+echo "=========="
+doPortScan $1
 echo "=========="
