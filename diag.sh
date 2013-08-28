54c89dee132355f53f17dbc2270afbbc648e53fb
Josh C <joshc@marvin.local>
2013-08-27 21:23:02 -0500

    added comments

--- diag.sh
+++ diag.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+#grab some quick diagnostics
+#
+#ping -o may not work on all systems
+#but seems to work ok on OSX
+#
+#Josh C 
+#https://github.com/irlrobot
+#
 
 function doPing {
   if ping -o $1 > /dev/null; then
@@ -12,6 +21,8 @@ function doLookup {
   nslookup -type=ANY $1
 }
 
+#add some common ports
+#in your environment
 function doPortScan {
   nmap -p T:22,25,80,443,4443,8000,8080 $1
 }
