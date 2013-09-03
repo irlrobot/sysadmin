3c997f881e8fda26499f8f71cbbe04dc809536c8
Josh C <jocampbell@GM19357.group.on>
2013-09-03 16:37:46 -0500

    initial commit
    ruby port of diag.sh

---
+++ diag.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+def doLookup(device)
+  puts `nslookup #{device}`  
+end
+
+def doPing(device)
+  puts `ping -c 2 #{device}`
+end
+
+def doPortScan(device)
+  puts `nmap -p T:22,25,80,443,4443,8000,8080 #{device}`  
+end
+
+input = ARGV[0].chomp
+
+puts "============================="
+puts "DNS Info"
+puts "============================="
+doLookup(input)
+puts "============================="
+puts "Ping Check"
+puts "============================="
+doPing(input)
+puts "============================="
+puts "Port Scan"
+puts "============================="
+doPortScan(input)
+puts "============================="
