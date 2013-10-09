95e54c36c0c23a7a3c7162d5aeb4b99a08b4ea00
Josh C <jocampbell@GM19357.group.on>
2013-10-09 09:53:09 -0500

    added comments

--- deploy.rb
+++ deploy.rb
@@ -1,10 +1,17 @@
 #!/usr/bin/ruby
+#
+# Expects hosts.txt and commands.txt in same directory
+# hosts.txt = list of hosts to run commands on (one per line)
+# commands.txt = commands to run on each host (one per line)
+#
+
 require 'rubygems'
 require 'net/ssh'
 
 File.open("hosts.txt").each_line do |host|
   puts "Deploying to #{host}"
 
+# chanage jocampbell to your username
   ssh = Net::SSH.start(host.chomp, "jocampbell")
 
   File.open("commands.txt").each_line do |command|
