f289aa14c495cabbab421c92aa7ef01a3180f4de
Josh C <jocampbell@GM19357.group.on>
2013-10-10 16:33:30 -0500

    specify username in hosts.txt
    you now specify the username within hosts.txt instead of hardcoding one
    username in the script. hosts.txt file format is now username space
    hostname
    e.g.
    josh splunk01.com
    root testbox02.com
    josh nagios02.com

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/ruby
 #
 # Expects hosts.txt and commands.txt in same directory
-# hosts.txt = list of hosts to run commands on (one per line)
-# commands.txt = commands to run on each host (one per line)
+# gem net-ssh should be installed
+#
+# hosts.txt
+# On each line put the username with access followed by a space and then the hostname
+# You should have public key authentication already setup on the host for the user specified
+# For example "josh splunk01.domain"
+#
+# commands.txt
+# Each command should be on one line, commands are run sequentially not in parrallel
+# For example "sudo yum update -y"
 # For sudo, requiretty must be disabled in visudo, or try deploy_channel.rb
 #
 
@@ -10,10 +18,12 @@ require 'rubygems'
 require 'net/ssh'
 
 File.open("hosts.txt").each_line do |host|
-  puts "Deploying to #{host}"
+  username = host.split[0]
+  hostname = host.split[1]
 
-# chanage jocampbell to your username
-  ssh = Net::SSH.start(host.chomp, "jocampbell")
+  puts "Deploying to #{hostname}"
+
+  ssh = Net::SSH.start(hostname, username)
 
   File.open("commands.txt").each_line do |command|
     puts "--------------------------------------------------"
