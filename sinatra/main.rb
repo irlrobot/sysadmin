0617b6c58c41df7912972054ba9af16da39cf255
Josh C <joshc@marvin.local>
2013-10-15 22:31:51 -0500

    sinatra testing

---
+++ sinatra/main.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  erb :home
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/contact' do
+  erb :contact
+end
+
+not_found do
+  erb :not_found
+end
