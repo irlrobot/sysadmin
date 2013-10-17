ca11a0d83f3c9999330f6dfb9f682d871990cecb
Josh C <jocampbell@GM19357.group.on>
2013-10-17 13:43:47 -0500

    finished chapter 3

--- sinatra/main.rb
+++ sinatra/main.rb
@@ -1,4 +1,5 @@
 #!/usr/local/bin/ruby
+require './song'
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' if development?
@@ -21,6 +22,42 @@ end
 get '/contact' do
   @title = "Contact Us"
   slim :contact
+end
+
+get '/songs' do
+  @songs = Song.all
+  slim :songs
+end
+
+get '/songs/new' do
+  @song = Song.new
+  slim :new_song
+end
+
+get '/songs/:id' do
+  @song = Song.get(params[:id])
+  slim :show_song
+end
+
+get '/songs/:id/edit' do
+  @song = Song.get(params[:id])
+  slim :edit_song
+end
+
+post '/songs' do
+  song = Song.create(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+put '/songs/:id' do
+  song = Song.get(params[:id])
+  song.update(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+delete '/songs/:id' do
+  Song.get(params[:id]).destroy
+  redirect to('/songs')
 end
 
 not_found do
