ad5cae5e3d3b3f5a612f632973242ecce2b3c6b4
Josh Campbell <josh@userdel.com>
2017-01-16 16:00:28 -0800

    adds cmder and zshrc for bash on windows

---
+++ bash_on_windows/cmder_config/user-aliases.cmd
@@ -0,0 +1,25 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+gh=cd /d d:\git
+ll=ls -Glah
+gb=git branch
+gm=git merge --no-ff
+gco=git checkout
+gs=git status
+gp=git pull --rebase
+bashhome=cd C:\Users\joshcb\AppData\Local\Lxss\home\josh
+# mkvenv='virtualenv -p /usr/local/bin/python3 venv'
+# activate='source venv/bin/activate'
